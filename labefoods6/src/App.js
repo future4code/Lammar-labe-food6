c8e43451116ab145afa85598b0e8bd116006bd3b
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-09-15 23:10:33 -0300

    teste

--- labefoods6/src/App.js
+++ labefoods6/src/App.js
@@ -1,7 +1,9 @@
+import Router from "./Routes/Router";
 
 function App() {
   return (
     <>
+      <Router />
     </>
   );
 }
--- labefoods6/src/Routes/Router.js
+++ labefoods6/src/Routes/Router.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-
-
-import Splash from '../Pages/Splash'
 import Login from '../Pages/Login'
 import SignUp from '../Pages/SignUp'
 import Busca from '../Pages/Busca'
@@ -18,19 +15,17 @@ const Router =()=>{
 
     return(
         <BrowserRouter>
-        <Routes>
-            <Route path='/'  element={<Splash/>}/>
-            <Route path='/login'  element={<Login/>}/>
-            <Route path='/signup' element={<SignUp/>}/>
-            <Route path='/buscar' element={<Busca/>}/>
-            <Route path='/cadastar' element={<Cadastro/>}/>
-            <Route path='/editar' element={<Editar/>}/>
-            <Route path='/feed' element={<Feed/>}/>
-            <Route path='/meucarrinho' element={<MeuCarrinho/>}/>
-            <Route path='/meuperfil' element={<MeuPerfil/>}/>
-            <Route path='/resultado' element={<Resultado/>}/>
-
-        </Routes>
+            <Routes>
+                <Route path='/login'  element={<Login />}/>
+                <Route path='/signup' element={<SignUp/>}/>
+                <Route path='/buscar' element={<Busca/>}/>
+                <Route path='/cadastar' element={<Cadastro/>}/>
+                <Route path='/editar' element={<Editar/>}/>
+                <Route path='/feed' element={<Feed/>}/>
+                <Route path='/meucarrinho' element={<MeuCarrinho/>}/>
+                <Route path='/meuperfil' element={<MeuPerfil/>}/>
+                <Route path='/resultado' element={<Resultado/>}/>
+            </Routes>
         </BrowserRouter>
     )
 }
