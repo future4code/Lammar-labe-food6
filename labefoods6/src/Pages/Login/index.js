2ec8f9487c0523d1c0244b01675d9a9b71348ba4
jarbasmarinho33 <jarbasmarinho33@gmail.com>
2022-09-15 22:53:07 -0300

    Rotas

---
+++ labefoods6/src/Pages/Login/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {goToSignUp} from '../../Routes/Coordinator'
+import { useNavigate } from 'react-router-dom'
+
+const Login =()=>{
+    
+        const navigate = useNavigate()
+    
+    return( 
+        <>
+        <h1>Entrar</h1>
+        <label htmlFor='email'>Email</label>
+        <input type="email" id='email'></input>
+        <label htmlFor='senha'>Senha</label>
+        <input type="password" id='senha'></input>  
+        <button>Entrar</button>   
+        Não possui cadastro? <label onClick={()=>goToSignUp(navigate)}>Clique aqui.</label>
+        </>
+    )
+}
+export default Login;
---
+++ labefoods6/src/Pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp=()=>{
+
+    return(
+        <>
+        <input></input>
+        </>
+        )
+}
+export default SignUp;
