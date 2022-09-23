2bf7d2d9c9e118a07bf52764da442f1e2b7c3aca
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-09-23 15:13:11 -0300

    SUBTOTAL na pagina de carrinho, só falta adicionar o frete

--- labefoods6/src/Pages/MeuCarrinho/index.js
+++ labefoods6/src/Pages/MeuCarrinho/index.js
@@ -1,21 +1,23 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { BotaoLaranja } from "../../Components/Botoes/Styled.js"
 import { CardItens, InformacaoProduto, Preco } from "../../Components/Cards/Styled"
 import { FooterComponents } from "../../Components/Footer/Footer.js"
 import { DivFundoPaginaFooter } from "../../Components/Footer/Styled"
 import { GlobalStateContext } from "../../Global/GlobalStateContext"
-import { TextoCarrinho } from "./styled"
+import { DivValorTotal, TextoCarrinho } from "./styled"
 
-export const MeuCarrinhoPage=()=>{
+export const MeuCarrinhoPage=(props)=>{
     const navigate=useNavigate()
     const {addProduto, setAddProduto, }=useContext(GlobalStateContext)
+    const [valorTotal, setValorTotal]=useState(0)
 
     const deletarProdutos = (produto) =>{
         const novoCarrinho = [...addProduto]
         const deletar = novoCarrinho.findIndex((p)=> p === produto)
         novoCarrinho.splice(deletar, 1)
         setAddProduto(novoCarrinho)
+
     }
 
     const carrinho = addProduto.map((item, index)=>{
@@ -26,7 +28,7 @@ export const MeuCarrinhoPage=()=>{
                     <p className="quantidade">{item.quantity}</p>
                     <p>{item.name}</p>
                     <p className="descricao">{item.description}</p>
-                    <Preco>
+                    <Preco> 
                         <span> {(item.price).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} </span>
                         <button onClick={()=>deletarProdutos(item)}>Remover</button>
                     </Preco>  
@@ -34,7 +36,15 @@ export const MeuCarrinhoPage=()=>{
             </CardItens>
         )
     })
-    console.log(carrinho)
+    useEffect(()=>{
+        let valorFinal = 0
+        addProduto.forEach((produto)=>{
+            valorFinal += (produto.price * produto.quantity) 
+        }, [addProduto])
+        setValorTotal(valorFinal)
+        localStorage.setItem("carrinho", JSON.stringify(addProduto))
+    })
+
     return(
         <DivFundoPaginaFooter>
             {carrinho.length > 0 ?
@@ -44,6 +54,10 @@ export const MeuCarrinhoPage=()=>{
             :
             <TextoCarrinho>Carrinho vazio</TextoCarrinho>
             }
+            <DivValorTotal>
+                <p className="subtotal">SUBTOTAL:</p> 
+                <p className="valor"> {valorTotal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+            </DivValorTotal>
             <BotaoLaranja>Confirmar</BotaoLaranja>
             <FooterComponents />
         </DivFundoPaginaFooter>
--- labefoods6/src/Pages/MeuCarrinho/styled.js
+++ labefoods6/src/Pages/MeuCarrinho/styled.js
@@ -16,4 +16,35 @@ export const TextoCarrinho=styled.p`
     margin: 8px 0 33px;
     padding: 12px 32px;
 `
+export const DivValorTotal=styled.div`
+    display: flex;
 
+    .subtotal{
+        width: 164px;
+        height: 18px;
+        margin: 15px 0 25px 16px;
+        font-family: Roboto;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.39px;
+        color: #000000;
+    }
+
+    .valor{
+        width: 164px;
+        height: 21px;
+        margin: 13px 16px 24px 0;
+        font-family: Roboto;
+        font-size: 18px;
+        font-weight: bold;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.43px;
+        text-align: right;
+        color: var(--dark-peach);
+    }
+`
